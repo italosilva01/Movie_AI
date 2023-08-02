1e30db8aa523c9223dc4d772e5726ac82b4e063b
Francisco Italo <italo.silvan87@gmail.com>
2023-08-02 02:44:47 -0300

    projeto finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,28 @@ import { Movie } from "@/components/Movie";
 
 export default function Home() {
   const movies = [
-    { name: "Barbie", postUrl: "", duration: "1:54:00", releaseYear: "2023" },
     {
-      name: "Oppenheimer",
-      postUrl: "",
-      duration: "3:00:00",
+      name: "Barbie",
+      postUrl: "/posts/barbier.png",
+      duration: "1:54:00",
       releaseYear: "2023",
     },
     {
       name: "Indiana Jones e o Chamado",
-      postUrl: "",
+      postUrl: "/posts/indianaJones.png",
       duration: "1:54:00",
+      releaseYear: "2023",
+    },
+    {
+      name: "Oppenheimer",
+      postUrl: "/posts/oppenheimer.png",
+      duration: "3:00:00",
       releaseYear: "2023",
     },
   ];
   return (
     <main className="flex flex-col items-center min-h-screen justify-center bg-gradient-to-l  to-[--purple] from-[--pink] p-26">
-      <div className="w-[52.875rem] h-[39.5rem] flex flex-col gap-[2.375rem] bg-[--gray-dark] mx-[18.5625rem] my-[8.3125rem] px-[6rem] py-[4rem] rounded-2xl boxShadowstyled ">
+      <div className="w-[52.875rem] h-[39.5rem] flex flex-col gap-[2.375rem] bg-[--gray-dark] mx-[18.5625rem] my-[8.3125rem] px-[6rem] py-[4rem] rounded-2xl boxShadowstyled border-8 border-[--purple]	">
         <div className="flex justify-between">
           <Image src="/logo.svg" width={86} height={44} alt={"Logo Movie AI"} />
 
@@ -30,9 +35,9 @@ export default function Home() {
         </div>
 
         <div className="lg:grid lg:grid-cols-3 lg:gap-x-9 sm:grid-cols-1 sm:gap-x-0 sm:gap-y-2 justify-center ">
-          <Movie />
-          <Movie />
-          <Movie />
+          {movies.map((movie) => (
+            <Movie movie={movie} />
+          ))}
         </div>
       </div>
     </main>
--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -1,35 +1,45 @@
 import Image from "next/image";
 import { ButtonPlay } from "../ButtonPlay";
 
-export const Movie = () => {
+interface MovieProps {
+  movie: {
+    name: string;
+    postUrl: string;
+    duration: string;
+    releaseYear: string;
+  };
+}
+export const Movie = ({ movie }: MovieProps) => {
+  const { name, duration, postUrl, releaseYear } = movie;
+
   return (
     <div className="w-[12.125rem] h-[25.0625rem] ">
       {/* --title */}
-      <div className="w-full flex justify-between justify-items-center	justify-self-center	mb-6">
-        <span className="text-white text-xl font-medium	leading-5 max-w-[8.75rem]	">
-          Barbie
+      <div className="w-full flex justify-between 	justify-self-center	mb-6 ">
+        <span className="text-white text-xl font-medium	leading-5 max-w-[8.75rem] h-[40px] text-ellipsis flex items-center 	">
+          {name}
         </span>
-        <div className="flex items-center	">
+        <div className="flex items-center align-top	">
           <Image width={16} height={15.42} src="/icons/star.png" alt={""} />
 
-          <span className="text-[--yellow] text-base font-semibold leading-[0.9005rem]">
+          <span className="text-[--yellow] text-base font-semibold leading-[0.9005rem] align-top">
             4.9
           </span>
         </div>
       </div>
       {/* post */}
       <div>
-        <Image alt={""} src="/posts/barbier.png" width={194} height={289} />
+        <Image alt={""} src={postUrl} width={194} height={289} />
       </div>
 
-      <div className="flex justify-between my-[0.5rem]">
+      <div className="flex justify-between my-[0.5rem] text-[--gray-light]">
         <div className="flex gap-[0.25rem]">
           <Image alt="" src="/icons/clock.svg" width={20} height={20} />
-          1:54:00
+          {duration}
         </div>
         <div className="flex gap-[0.25rem]">
           <Image alt="" src="/icons/CalendarBlank.svg" width={20} height={20} />
-          2023
+          {releaseYear}
         </div>
       </div>
       <ButtonPlay />
